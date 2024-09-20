b614d82847fd687515b063578f547126bfcf699f
chrisisaasccc <ctw20.chris.isaac@corketb.ie>
2024-09-20 13:40:31 +0100

    Add files via upload

---
+++ task 1-5 homework.py
@@ -0,0 +1,51 @@
+#author:chris isaac
+#date:9/09/24
+#homework
+
+#Get age
+age = 25 
+print("(age) - What a great age!")
+num1 = 5.7
+num2 = 3.2
+
+sum_result = num1 + num2
+print("Sum: (sum_result)")
+
+difference_result = num1 - num2
+print("Difference: (difference_result)")
+
+product_result = num1 * num2
+print("Product: (product_result)")
+
+num1 = 7.5
+num2 = 2.3
+
+average = (num1 + num2) / 2
+print("Average: (average)")
+
+remainder = num1 % num2
+print("Remainder: (remainder)")
+
+power = num1 ** num2
+print("(num1) to the power of (num2) is: (power)")
+
+fahrenheit = 87
+
+celsius = (5/9) * (fahrenheit - 32)
+
+celsius_rounded = round(celsius)
+
+
+print("The temperature in Chicago, (fahrenheit)°F, is approximately (celsius_rounded)°C.")
+distance_miles = 10000 
+
+
+distance_km = distance_miles * 1.60935
+
+distance_km_rounded = round(distance_km)
+
+cost_per_km = 900  
+total_cost = distance_km_rounded * cost_per_km
+
+print("The distance from your school to Singapore is approximately (distance_km_rounded) km.")
+print("The total cost of the helicopter flight is (total_cost).")
